af6b59dec2f2430a6b40899b51e71f7d6ce0a0e7
Somogyi Tibor <somogyi.tibor1997@gmail.com>
2025-02-10 10:25:08 +0100

    bugfix/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL, // Fix: `databaseURL` a megfelelő kulcs
+};
 
-const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    /*Take my:*/
-    reactDatabaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
-})
+// Firebase inicializálása
+const app = initializeApp(firebaseConfig);
 
-export const auth = app.auth()
+// Hitelesítés exportálása
+export const auth = getAuth(app);
 export default app;
